dccdc3c19b9144ce4d73ca6ba3ef78975b2fa9d4
lopezalbertogabriel <lopezalbertogabriel@gmail.com>
2017-04-04 10:14:14 -0300

    Commiteo textyra del terreno, heightmap y skybox

--- TGC.Group/Model/Shooter.cs
+++ TGC.Group/Model/Shooter.cs
@@ -13,7 +13,7 @@ namespace TGC.Group.Model
         private string heightmapDir;
         private string terrainTextureDir;
         private TgcSimpleTerrain terreno;
-
+		private TgcSkyBox skyBox;
 
         /// <summary>
         ///     Constructor del juego.
@@ -29,18 +29,34 @@ namespace TGC.Group.Model
 
         public override void Init()
         {
+			//Crear SkyBox
+			skyBox = new TgcSkyBox();
+			skyBox.Center = new Vector3(0, 500, 0);
+			skyBox.Size = new Vector3(8000, 8000, 8000);
+			var texturesPath = MediaDir + "Texturas\\Quake\\SkyBox LostAtSeaDay\\";
+		
+			skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Up, texturesPath + "lostatseaday_up.jpg");
+			skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Down, texturesPath + "lostatseaday_dn.jpg");
+			skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Left, texturesPath + "lostatseaday_lf.jpg");
+			skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Right, texturesPath + "lostatseaday_rt.jpg");
+			skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Front, texturesPath + "lostatseaday_bk.jpg");
+			skyBox.setFaceTexture(TgcSkyBox.SkyFaces.Back, texturesPath + "lostatseaday_ft.jpg");
+			skyBox.Init();
+
             //Device de DirectX para crear primitivas.
             var d3dDevice = D3DDevice.Instance.Device;
 
-            Camara = new FirstPersonCamera(Input);
+			Camara = new FirstPersonCamera(new Vector3(0, 1500, 0), Input);
 
-            heightmapDir = MediaDir + "Heightmaps\\" + "arenaheightmap.jpg";
-            terrainTextureDir = MediaDir + "Texturas\\" +  "arena.jpg";
+            heightmapDir = MediaDir + "Heightmaps\\" + "heightmap_v1.jpg";
+            terrainTextureDir = MediaDir + "Texturas\\" +  "map_v2.jpg";
 
             terreno = new TgcSimpleTerrain();
-            terreno.loadHeightmap(heightmapDir, 5, 2, Camara.LookAt);
+			terreno.loadHeightmap(heightmapDir, 15, 3, Camara.LookAt);
             terreno.loadTexture(terrainTextureDir);
-        }
+
+
+		}
 
         public override void Update()
         {
@@ -51,6 +67,8 @@ namespace TGC.Group.Model
         {
             //Inicio el render de la escena, para ejemplos simples. Cuando tenemos postprocesado o shaders es mejor realizar las operaciones según nuestra conveniencia.
             PreRender();
+
+			skyBox.render();
 
             terreno.render();
 
