a20e9b33d5c3e35114c60c4d8d35b4eedabde072
Francisco Enzo Di Giorgio <f.e.digiorgio@gmail.com>
2017-04-24 23:44:43 -0300

    Implementado obb (se mueve y rota) todo: colisiones con obb

--- TGC.Group/Model/GameModel.cs
+++ TGC.Group/Model/GameModel.cs
@@ -12,7 +12,9 @@ using TGC.Core.Utils;
 using TGC.Camara;
 using TGC.Core.Collision;
 using TGC.UtilsGroup;
+using TGC.Core.Geometry;
 using System;
+using TGC.Core.BoundingVolumes;
 
 namespace TGC.Group.Model
 {
@@ -80,6 +82,9 @@ namespace TGC.Group.Model
 
         //Mesh del auto
         private TgcMesh Mesh { get; set; }
+
+        //BoudingBox Obb del auto.
+        private TgcBoundingOrientedBox ObbMesh;
 
         //Boleano para ver si dibujamos el boundingbox
         private bool BoundingBox { get; set; }
@@ -233,6 +238,9 @@ namespace TGC.Group.Model
             Mesh.AutoTransformEnable = true;
             Mesh.move(0, 0.5f, 0);
             piso = Mesh.Position.Y;
+
+            //Cargo el bouding box obb del auto a partir de su AABB
+            ObbMesh = TgcBoundingOrientedBox.computeFromAABB(Mesh.BoundingBox);
 
             //Camara por defecto
             CamaraInterna = new TgcThirdPersonCamera(Mesh.Position, 300, 600);
@@ -606,8 +614,7 @@ namespace TGC.Group.Model
             DrawText.drawText("Con la tecla F se dibuja el bounding box.", 0, 20, Color.Red);
             DrawText.drawText("Con la tecla F1 se cambia el tipo de camara. Pos [Actual]: " + TgcParserUtils.printVector3(Camara.Position), 0, 30, Color.Red);
 
-            DrawText.drawText("Jugador 1: " + this.NombreJugador1, 1200, 10, Color.LightYellow);
-            DrawText.drawText("Velocidad: " + this.MOVEMENT_SPEED, 1200, 20, Color.LightYellow);
+            DrawText.drawText("Velocidad: " + this.MOVEMENT_SPEED, 1200, 10, Color.LightYellow);
 
             //Siempre antes de renderizar el modelo necesitamos actualizar la matriz de transformacion.
             //Debemos recordar el orden en cual debemos multiplicar las matrices, en caso de tener modelos jerárquicos, tenemos control total.
@@ -633,9 +640,10 @@ namespace TGC.Group.Model
             {
                 foreach (var mesh in ScenePpal.Meshes)
                 {
-                    mesh.BoundingBox.render();
+                    if(mesh.Equals(Mesh) == false) mesh.BoundingBox.render(); //agrego este if asi no dibujamos al bounding box AABB
                 }
 
+                ObbMesh.render();
                 RenderizarObjetos(1);
             }
 
@@ -910,6 +918,7 @@ namespace TGC.Group.Model
                 var rotAngle = (rotate * ElapsedTime) * (FastMath.PI / 180);
                 Mesh.rotateY(rotAngle);
                 CamaraInterna.rotateY(rotAngle);
+                ObbMesh.rotate(new Vector3(0, rotAngle, 0));
             }
 
             if (!falling)
@@ -947,6 +956,7 @@ namespace TGC.Group.Model
 
                 movement *= MOVEMENT_SPEED * ElapsedTime;
                 Mesh.moveOrientedY(moveForward * ElapsedTime);
+                ObbMesh.Center = Mesh.Position;
 
                 //El framework posee la clase TgcCollisionUtils con muchos algoritmos de colisión de distintos tipos de objetos.
                 //Por ejemplo chequear si dos cajas colisionan entre sí, o dos esferas, o esfera con caja, etc.
