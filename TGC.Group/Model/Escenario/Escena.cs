28f639de255b84cdb936c26226f45a512ced3b89
Sebastian Ciancio <sebastian.ciancio@gmail.com>
2017-08-27 19:09:17 -0300

    Escenario con Isla cargada...

--- TGC.Group/Model/Escenario/Escena.cs
+++ TGC.Group/Model/Escenario/Escena.cs
@@ -21,25 +21,18 @@ namespace TGC.Group.Model
     // Objeto Escena
     public class Escena
     {
-        private string MediaDir;
-        private TgcSceneLoader loader;
-        private GameModel env;
+        // ************************************************************************
+        // Declaracion de Variables
+        // ************************************************************************
 
-        private new TgcD3dInput Input { get; set; }
+            private string MediaDir;
+            private TgcSceneLoader loader;
+            private GameModel env;
+            private new TgcD3dInput Input { get; set; }
 
+            private TgcScene currentScene;
 
-        //Caja que se muestra en el ejemplo.
-        private TgcBox Box { get; set; }
-
-        private TgcMesh Mesh { get; set; }
-
-
-        //Constantes para velocidades de movimiento
-        private const float ROTATION_SPEED = 0.5f;
-        private const float MOVEMENT_SPEED = 5f;
-        private int TiempoRetardo = 3;
-        private int contadorDeCiclos = 0;
-        private float currentMoveDir = 1f;
+        // ************************************************************************
 
         private static Escena myInstance;
 
@@ -57,32 +50,14 @@ namespace TGC.Group.Model
             var d3dDevice = D3DDevice.Instance.Device;
 
 
-            //Cargamos una textura, tener en cuenta que cargar una textura significa crear una copia en memoria.
-            //Es importante cargar texturas en Init, si se hace en el render loop podemos tener grandes problemas si instanciamos muchas.
-            var texture = TgcTexture.createTexture(MediaDir + Game.Default.TexturaCaja);
+            //Cargar escena con herramienta TgcSceneLoader
+            var loader = new TgcSceneLoader();
+            currentScene = loader.loadSceneFromFile(MediaDir + "Isla\\isla-TgcScene.xml");
 
-            //Creamos una caja 3D ubicada de dimensiones (5, 10, 5) y la textura como color.
-            var size = new Vector3(5, 10, 5);
-            //Construimos una caja según los parámetros, por defecto la misma se crea con centro en el origen y se recomienda así para facilitar las transformaciones.
-            Box = TgcBox.fromSize(size, texture);
-
-            //Posición donde quiero que este la caja, es común que se utilicen estructuras internas para las transformaciones.
-            //Entonces actualizamos la posición lógica, luego podemos utilizar esto en render para posicionar donde corresponda con transformaciones.
-            Box.Position = new Vector3(-25, 0, 0);
-
-
-
-            //Cargar mesh
-            Mesh = new TgcSceneLoader().loadSceneFromFile(MediaDir + "LogoTGC-TgcScene.xml").Meshes[0];
-
-            //Defino una escala en el modelo logico del mesh que es muy grande.
-            Mesh.Scale = new Vector3(0.5f, 0.5f, 0.5f);
-
-            //Centrar camara rotacional respecto a este mesh
-            var Camara = new TgcRotationalCamera(Mesh.BoundingBox.calculateBoxCenter(),
-                Mesh.BoundingBox.calculateBoxRadius() * 2, Input);
+            //Ajustar camara en base al tamano del objeto
+            var Camara = new TgcRotationalCamera(currentScene.BoundingBox.calculateBoxCenter(),
+                currentScene.BoundingBox.calculateBoxRadius() * 2, Input);
         }
-
 
         public void Update()
         {
@@ -91,30 +66,14 @@ namespace TGC.Group.Model
 
         public void Render()
         {
-            //Cuando tenemos modelos mesh podemos utilizar un método que hace la matriz de transformación estándar.
-            //Es útil cuando tenemos transformaciones simples, pero OJO cuando tenemos transformaciones jerárquicas o complicadas.
-            //Mesh.UpdateMeshTransform();
-            //Render del mesh
-            //Mesh.render();
-
-            //Siempre antes de renderizar el modelo necesitamos actualizar la matriz de transformacion.
-            //Debemos recordar el orden en cual debemos multiplicar las matrices, en caso de tener modelos jerárquicos, tenemos control total.
-            Box.Transform = Matrix.Scaling(Box.Scale) *
-                            Matrix.RotationYawPitchRoll(Box.Rotation.Y, Box.Rotation.X, Box.Rotation.Z) *
-                            Matrix.Translation(Box.Position);
-            //A modo ejemplo realizamos toda las multiplicaciones, pero aquí solo nos hacia falta la traslación.
-            //Finalmente invocamos al render de la caja
-            Box.render();
+            //Renderizar escena entera sin mostrar los contornos
+            currentScene.renderAll(false);
 
         }
         public void dispose()
         {
-
             //Dispose del mesh.
-            //Mesh.dispose();
-
-            //Dispose de la caja.
-            Box.dispose();
+            currentScene.disposeAll();
         }
 
        
