85f79dc7cf5e831c1bb460883edad2f95e72424b
gerosamore <cortigeronimo@gmail.com>
2018-04-13 18:37:13 -0300

    configuracion inicial realizada

--- TGC.Group/Game.Designer.cs
+++ TGC.Group/Game.Designer.cs
@@ -37,7 +37,7 @@ namespace TGC.Group {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Grupo 99")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Cutucuchillo")]
         public string Name {
             get {
                 return ((string)(this["Name"]));
@@ -49,7 +49,7 @@ namespace TGC.Group {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Mi idea - Descripción de la idea")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Twisted Metal – Autos chocadores / Derby de demolición")]
         public string Description {
             get {
                 return ((string)(this["Description"]));
