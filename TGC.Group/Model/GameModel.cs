cc6c2c06ecfeba3d1b955cbc16960a30e0a36097
soriac <sbcristiansoria@gmail.com>
2018-05-04 23:59:10 -0300

    Separo al personaje en su propia clase.

--- TGC.Group/Model/GameModel.cs
+++ TGC.Group/Model/GameModel.cs
@@ -1,117 +1,35 @@
 using Microsoft.DirectX.DirectInput;
-using TGC.Core.Direct3D;
 using TGC.Core.Example;
 using TGC.Core.Mathematica;
 using TGC.Core.SceneLoader;
-using TGC.Core.SkeletalAnimation;
 
 namespace TGC.Group.Model {
-    /// <summary>
-    ///     Ejemplo para implementar el TP.
-    ///     Inicialmente puede ser renombrado o copiado para hacer más ejemplos chicos, en el caso de copiar para que se
-    ///     ejecute el nuevo ejemplo deben cambiar el modelo que instancia GameForm <see cref="Form.GameForm.InitGraphics()" />
-    ///     line 97.
-    /// </summary>
-    public class GameModel : TgcExample
-    {
-        /// <summary>
-        ///     Constructor del juego.
-        /// </summary>
-        /// <param name="mediaDir">Ruta donde esta la carpeta con los assets</param>
-        /// <param name="shadersDir">Ruta donde esta la carpeta con los shaders</param>
-        public GameModel(string mediaDir, string shadersDir) : base(mediaDir, shadersDir)
-        {
+    public class GameModel : TgcExample {
+        public GameModel(string mediaDir, string shadersDir) : base(mediaDir, shadersDir) {
             Category = Game.Default.Category;
             Name = Game.Default.Name;
             Description = Game.Default.Description;
         }
 
-        // Mesh del jugador.
-        private TgcSkeletalMesh personaje;
-        private TGCVector3 velocidadPersonaje;
-
         // Escena de la ciudad.
         private TgcScene scene;
 
+        private Personaje personaje;
         private TGCVector3 cameraOffset;
-        private const float WALK_SPEED = 5f;
-        private bool moving = false;
+
 
         public override void Init() {
-            //Device de DirectX para crear primitivas.
-            var d3dDevice = D3DDevice.Instance.Device;
-
-            var skeletalLoader = new TgcSkeletalLoader();
-            personaje = skeletalLoader.loadMeshAndAnimationsFromFile(
-
-
-                MediaDir + "Robot\\Robot-TgcSkeletalMesh.xml",
-                MediaDir + "Robot\\",
-                new[]
-                {
-                     MediaDir + "Robot\\Caminando-TgcSkeletalAnim.xml",
-                     MediaDir + "Robot\\Parado-TgcSkeletalAnim.xml"
-                });
-
-            personaje.buildSkletonMesh();
-            personaje.playAnimation("Parado", true);
-            personaje.Scale = new TGCVector3(0.5f, 0.5f, 0.5f);
-            personaje.Position = new TGCVector3(300, -85, 0);
-
             scene = new TgcSceneLoader().loadSceneFromFile(MediaDir + "Jungla\\Jungla -TgcScene.xml");
-
             cameraOffset = new TGCVector3(0, 200, 400);
-
-            velocidadPersonaje = TGCVector3.Empty;
+            personaje = new Personaje(MediaDir);
         }
 
 
         public override void Update() {
             PreUpdate();
 
-            Camara.SetCamera(personaje.Position + cameraOffset, personaje.Position);
-
-            if (Input.keyDown(Key.W) || Input.keyDown(Key.UpArrow)) {
-                velocidadPersonaje.Z = -WALK_SPEED;
-                moving = true;
-            }
-
-            if (Input.keyDown(Key.S) || Input.keyDown(Key.DownArrow)) {
-                velocidadPersonaje.Z = WALK_SPEED;
-                moving = true;
-            }
-
-            if (Input.keyDown(Key.D) || Input.keyDown(Key.RightArrow)) {
-                velocidadPersonaje.X = -WALK_SPEED;
-                moving = true;
-            }
-
-            if (Input.keyDown(Key.A) || Input.keyDown(Key.LeftArrow)) {
-                velocidadPersonaje.X = WALK_SPEED;
-                moving = true;
-            }
-
-            if (Input.keyDown(Key.LeftShift)) {
-                velocidadPersonaje.X = velocidadPersonaje.X * 2;
-                velocidadPersonaje.Z = velocidadPersonaje.Z * 2;
-            } else if (Input.keyDown(Key.LeftAlt)) {
-                velocidadPersonaje.X = velocidadPersonaje.X / 2;
-                velocidadPersonaje.Z = velocidadPersonaje.Z / 2;
-            }
-
-            if (moving) {
-                personaje.playAnimation("Caminando", true);
-            } else {
-                personaje.playAnimation("Parado", true);
-            }
-
-            //Aplicar movimiento hacia adelante o atras segun la orientacion actual del Mesh
-            var lastPos = personaje.Position;
-
-            personaje.Move(velocidadPersonaje);
-            velocidadPersonaje.X = 0;
-            velocidadPersonaje.Z = 0;
-            moving = false;
+            Camara.SetCamera(personaje.getPosition() + cameraOffset, personaje.getPosition());
+            personaje.update(ElapsedTime, Input);
 
             PostUpdate();
         }
@@ -121,19 +39,14 @@ namespace TGC.Group.Model {
             PreRender();
 
             scene.RenderAll();
-
-            //Cuando tenemos modelos mesh podemos utilizar un método que hace la matriz de transformación estándar.
-            //Es útil cuando tenemos transformaciones simples, pero OJO cuando tenemos transformaciones jerárquicas o complicadas.
-            personaje.UpdateMeshTransform();
-            //Render del mesh
-            personaje.animateAndRender(ElapsedTime);
+            personaje.render(ElapsedTime);
 
             //Finaliza el render y presenta en pantalla, al igual que el preRender se debe para casos puntuales es mejor utilizar a mano las operaciones de EndScene y PresentScene
             PostRender();
         }
 
         public override void Dispose() {
-            personaje.Dispose();
+            personaje.dispose();
             scene.DisposeAll();
         }
     }
---
+++ TGC.Group/Model/Personaje.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.DirectX.DirectInput;
+using TGC.Core.Input;
+using TGC.Core.Mathematica;
+using TGC.Core.SkeletalAnimation;
+
+namespace TGC.Group.Model {
+    class Personaje {
+        private TGCVector3 vel;
+        private TgcSkeletalMesh mesh;
+        private bool moving = false;
+        private const float WALK_SPEED = 5f;
+
+        public Personaje(string MediaDir) {
+            vel = TGCVector3.Empty;
+            var skeletalLoader = new TgcSkeletalLoader();
+
+            mesh = skeletalLoader.loadMeshAndAnimationsFromFile(
+                MediaDir + "Robot\\Robot-TgcSkeletalMesh.xml",
+                MediaDir + "Robot\\",
+                new[] {
+                     MediaDir + "Robot\\Caminando-TgcSkeletalAnim.xml",
+                     MediaDir + "Robot\\Parado-TgcSkeletalAnim.xml"
+                }
+            );
+
+            mesh.buildSkletonMesh();
+            mesh.playAnimation("Parado", true);
+            mesh.Scale = new TGCVector3(0.5f, 0.5f, 0.5f);
+            mesh.Position = new TGCVector3(300, -85, 0);
+        }
+
+        public void update(float deltaTime, TgcD3dInput Input) {
+            if (Input.keyDown(Key.W) || Input.keyDown(Key.UpArrow)) {
+                vel.Z = -WALK_SPEED;
+                moving = true;
+            }
+
+            if (Input.keyDown(Key.S) || Input.keyDown(Key.DownArrow)) {
+                vel.Z = WALK_SPEED;
+                moving = true;
+            }
+
+            if (Input.keyDown(Key.D) || Input.keyDown(Key.RightArrow)) {
+                vel.X = -WALK_SPEED;
+                moving = true;
+            }
+
+            if (Input.keyDown(Key.A) || Input.keyDown(Key.LeftArrow)) {
+                vel.X = WALK_SPEED;
+                moving = true;
+            }
+
+            if (Input.keyDown(Key.LeftShift)) {
+                vel.X = vel.X * 2;
+                vel.Z = vel.Z * 2;
+            } else if (Input.keyDown(Key.LeftAlt)) {
+                vel.X = vel.X / 2;
+                vel.Z = vel.Z / 2;
+            }
+
+            if (moving) {
+                mesh.playAnimation("Caminando", true);
+            } else {
+                mesh.playAnimation("Parado", true);
+            }
+
+            //Aplicar movimiento hacia adelante o atras segun la orientacion actual del Mesh
+            var lastPos = mesh.Position;
+
+            mesh.Move(vel);
+            vel.X = 0;
+            vel.Z = 0;
+            moving = false;
+        }
+
+        public void render(float deltaTime) {
+            mesh.UpdateMeshTransform();
+            mesh.animateAndRender(deltaTime);
+        }
+
+        public void dispose() {
+            mesh.Dispose();
+        }
+
+        public TGCVector3 getPosition() {
+            return mesh.Position;
+        }
+
+
+    }
+}
