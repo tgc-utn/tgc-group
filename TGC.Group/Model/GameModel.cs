2ca67bf280d9baa12a9a551c94644047d8d2949b
soriac <sbcristiansoria@gmail.com>
2018-05-04 23:41:44 -0300

    Vuelvo a la cámara anterior.

--- TGC.Group/Model/GameModel.cs
+++ TGC.Group/Model/GameModel.cs
@@ -1,18 +1,11 @@
 using Microsoft.DirectX.DirectInput;
-using Microsoft.DirectX.Direct3D;
-using System.Drawing;
-using System.Linq;
 using TGC.Core.Direct3D;
 using TGC.Core.Example;
-using TGC.Core.Geometry;
-using TGC.Core.Input;
 using TGC.Core.Mathematica;
 using TGC.Core.SceneLoader;
 using TGC.Core.SkeletalAnimation;
-using TGC.Core.Textures;
 
-namespace TGC.Group.Model
-{
+namespace TGC.Group.Model {
     /// <summary>
     ///     Ejemplo para implementar el TP.
     ///     Inicialmente puede ser renombrado o copiado para hacer más ejemplos chicos, en el caso de copiar para que se
@@ -33,32 +26,18 @@ namespace TGC.Group.Model
             Description = Game.Default.Description;
         }
 
-        private Cam camaraInterna;
-
-        float convertirARad(float angulo)
-        {
-            return FastMath.PI * angulo / 180f; 
-        }
-
         // Mesh del jugador.
         private TgcSkeletalMesh personaje;
+        private TGCVector3 velocidadPersonaje;
 
         // Escena de la ciudad.
         private TgcScene scene;
 
-        float piValue = FastMath.PI;
-        float quarterPi = FastMath.PI;
-        float halfPi = FastMath.PI_HALF;
-        string movementOrientation = "Forward";
+        private TGCVector3 cameraOffset;
+        private const float WALK_SPEED = 5f;
+        private bool moving = false;
 
-        /// <summary>
-        ///     Se llama una sola vez, al principio cuando se ejecuta el ejemplo.
-        ///     Escribir aquí todo el código de inicialización: cargar modelos, texturas, estructuras de optimización, todo
-        ///     procesamiento que podemos pre calcular para nuestro juego.
-        ///     Borrar el codigo ejemplo no utilizado.
-        /// </summary>
-        public override void Init()
-        {
+        public override void Init() {
             //Device de DirectX para crear primitivas.
             var d3dDevice = D3DDevice.Instance.Device;
 
@@ -81,243 +60,55 @@ namespace TGC.Group.Model
 
             scene = new TgcSceneLoader().loadSceneFromFile(MediaDir + "Jungla\\Jungla -TgcScene.xml");
 
-            camaraInterna = new Cam(personaje.Position, 140, 300);
-            var lookAt = TGCVector3.Empty;
-            Camara = camaraInterna;
+            cameraOffset = new TGCVector3(0, 200, 400);
+
+            velocidadPersonaje = TGCVector3.Empty;
         }
 
 
-        /// <summary>
-        ///     Se llama en cada frame.
-        ///     Se debe escribir toda la lógica de computo del modelo, así como también verificar entradas del usuario y reacciones
-        ///     ante ellas.
-        /// </summary>
-
-        public override void Update()
-        {
+        public override void Update() {
             PreUpdate();
 
-            var moving = false;
+            Camara.SetCamera(personaje.Position + cameraOffset, personaje.Position);
 
-            TGCVector3 movement = new TGCVector3(0, 0, 0);
-
-
-            /* if (Input.keyDown(Key.UpArrow) || Input.keyDown(Key.W))
-             {
-                 if (movementOrientation != "Forward")
-                 {
-                     if (movementOrientation == "Backwards")
-                     {
-                         personaje.RotateY(piValue);
-                         movementOrientation = "Forward";
-                         camaraInterna.rotateY(180f * ElapsedTime);
-
-                     }
-
-                     else if (movementOrientation == "Left")
-                     {
-                         personaje.RotateY(halfPi);
-                         movementOrientation = "Forward";
-                         camaraInterna.rotateY(90f * ElapsedTime);
-                     }
-
-                     else if (movementOrientation == "Right")
-                     {
-                         personaje.RotateY(-halfPi);
-                         movementOrientation = "Forward";
-                         camaraInterna.rotateY(-90f * ElapsedTime);
-                     }
-                 }
-
-                 movement.Z -= 1;
-                 moving = true;
-             }
-
-             else if (Input.keyDown(Key.DownArrow) || Input.keyDown(Key.S))
-             {
-                 if (movementOrientation != "Backwards")
-                 {
-                     if (movementOrientation == "Forward")
-                     {
-                         personaje.RotateY(piValue);
-                         movementOrientation = "Backwards";
-                         camaraInterna.rotateY(180f * ElapsedTime);
-                     }
-
-                     else if (movementOrientation == "Left")
-                     {
-                         personaje.RotateY(-halfPi);
-                         movementOrientation = "Backwards";
-                         camaraInterna.rotateY(-90f * ElapsedTime);
-                     }
-
-                     else if (movementOrientation == "Right")
-                     {
-                         personaje.RotateY(halfPi);
-                         movementOrientation = "Backwards";
-                         camaraInterna.rotateY(90f * ElapsedTime);
-                     }
-                 }
-
-                 movement.Z += 1;
-                 moving = true;
-             }
-
-             else if (Input.keyDown(Key.RightArrow) || Input.keyDown(Key.D))
-             {
-                 if (movementOrientation != "Right")
-                 {
-                     if (movementOrientation == "Backwards")
-                     {
-                         personaje.RotateY(-halfPi);
-                         movementOrientation = "Right";
-                         camaraInterna.rotateY(-90f * ElapsedTime);
-                     }
-
-                     else if (movementOrientation == "Left")
-                     {
-                         personaje.RotateY(-piValue);
-                         movementOrientation = "Right";
-                         camaraInterna.rotateY(180f * ElapsedTime);
-                     }
-
-                     else if (movementOrientation == "Forward")
-                     {
-                         personaje.RotateY(halfPi);
-                         movementOrientation = "Right";
-                         camaraInterna.rotateY(90f * ElapsedTime);
-                     }
-                 }
-
-                 movement.X -= 1;
-                 moving = true;
-             }
-
-             else if (Input.keyDown(Key.LeftArrow) || Input.keyDown(Key.A))
-             {
-                 if (movementOrientation != "Left")
-                 {
-                     if (movementOrientation == "Backwards")
-                     {
-                         personaje.RotateY(halfPi);
-                         movementOrientation = "Left";
-                         camaraInterna.rotateY(90f * ElapsedTime);
-                     }
-
-                     else if (movementOrientation == "Forward")
-                     {
-                         personaje.RotateY(-halfPi);
-                         movementOrientation = "Left";
-                         camaraInterna.rotateY(-90f * ElapsedTime);
-                     }
-
-                     else if (movementOrientation == "Right")
-                     {
-                         personaje.RotateY(piValue);
-                         movementOrientation = "Left";
-                         camaraInterna.rotateY(180f * ElapsedTime);
-                     }
-                 }
-
-                 movement.X += 1;
-                 moving = true;
-             }
-
-             if (moving)
-             {
-
-                 personaje.playAnimation("Caminando", true);
-             }
-
-             else
-             {
-
-                 personaje.playAnimation("Parado", true);
-             }
-
-             movement = movement * 220f * ElapsedTime;
-             personaje.Move(movement);
-     */
-
-            var velocidadCaminar = 50000f * ElapsedTime;
-            var velocidadRotacion = 130f;
-
-            var moveForward = 0f;
-            float rotate = 0;
-            var rotating = false;
-
-            //Adelante
-            if (Input.keyDown(Key.W) || Input.keyDown(Key.UpArrow)) //Ritmo normal
-            {
-                moveForward = -velocidadCaminar;
-                if (Input.keyDown(Key.LeftShift)) //Correr, le duplica la velocidad
-                {
-                    moveForward *= 2;
-                }
-                if (Input.keyDown(Key.CapsLock)) //Caminar, le reduce la velocidad a un cuarto
-                {
-                    moveForward /= 4;
-                }
+            if (Input.keyDown(Key.W) || Input.keyDown(Key.UpArrow)) {
+                velocidadPersonaje.Z = -WALK_SPEED;
                 moving = true;
             }
 
-            //Atras
-            if (Input.keyDown(Key.S) || Input.keyDown(Key.DownArrow))
-            {
-                moveForward = velocidadCaminar;
+            if (Input.keyDown(Key.S) || Input.keyDown(Key.DownArrow)) {
+                velocidadPersonaje.Z = WALK_SPEED;
                 moving = true;
             }
 
-            //Derecha
-            if (Input.keyDown(Key.D) || Input.keyDown(Key.RightArrow))
-            {
-                rotate = velocidadRotacion;
-                rotating = true;
+            if (Input.keyDown(Key.D) || Input.keyDown(Key.RightArrow)) {
+                velocidadPersonaje.X = -WALK_SPEED;
+                moving = true;
             }
 
-            //Izquierda
-            if (Input.keyDown(Key.A) || Input.keyDown(Key.LeftArrow))
-            {
-                rotate = -velocidadRotacion;
-                rotating = true;
+            if (Input.keyDown(Key.A) || Input.keyDown(Key.LeftArrow)) {
+                velocidadPersonaje.X = WALK_SPEED;
+                moving = true;
             }
 
-            //Si hubo rotacion
-            if (rotating)
-            {
-                //Rotar personaje y la camara, hay que multiplicarlo por el tiempo transcurrido para no atarse a la velocidad el hardware
-                var rotAngle = convertirARad(rotate * ElapsedTime);
-                personaje.RotateY(rotAngle);
-                camaraInterna.rotateY(rotAngle);
-            }
-
-            //Si hubo desplazamiento
-            if (moving)
-            {
-                //Activar animacion de caminando
+            if (moving) {
                 personaje.playAnimation("Caminando", true);
-            }
-
-            else
-            {
+            } else {
                 personaje.playAnimation("Parado", true);
             }
-                //Aplicar movimiento hacia adelante o atras segun la orientacion actual del Mesh
-                var lastPos = personaje.Position;
 
-                personaje.MoveOrientedY(moveForward * ElapsedTime);
+            //Aplicar movimiento hacia adelante o atras segun la orientacion actual del Mesh
+            var lastPos = personaje.Position;
 
-                camaraInterna.Target = personaje.Position;
+            personaje.Move(velocidadPersonaje);
+            velocidadPersonaje.X = 0;
+            velocidadPersonaje.Z = 0;
+            moving = false;
 
-                PostUpdate();
-            }
-        /// <summary>
-        ///     Se llama cada vez que hay que refrescar la pantalla.
-        ///     Escribir aquí todo el código referido al renderizado.
-        ///     Borrar todo lo que no haga falta.
-        /// </summary>
-        public override void Render()
-        {
+            PostUpdate();
+        }
+
+        public override void Render() {
             //Inicio el render de la escena, para ejemplos simples. Cuando tenemos postprocesado o shaders es mejor realizar las operaciones según nuestra conveniencia.
             PreRender();
 
@@ -333,13 +124,7 @@ namespace TGC.Group.Model
             PostRender();
         }
 
-        /// <summary>
-        ///     Se llama cuando termina la ejecución del ejemplo.
-        ///     Hacer Dispose() de todos los objetos creados.
-        ///     Es muy importante liberar los recursos, sobretodo los gráficos ya que quedan bloqueados en el device de video.
-        /// </summary>
-        public override void Dispose()
-        {
+        public override void Dispose() {
             personaje.Dispose();
             scene.DisposeAll();
         }
