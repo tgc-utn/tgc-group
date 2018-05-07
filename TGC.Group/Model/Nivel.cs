9f7d3ebb2606194d2a7f35d57b5c75b5d8e10dcf
pepeciavirella <pep.ciavirella@gmail.com>
2018-05-07 04:50:57 -0300

    Agregado el precipicio; falta configurarlo como deathplane (ver abstracción) y corregir desplazamiento tronco+tgcito
    Signed-off-by: pepeciavirella <pep.ciavirella@gmail.com>

--- TGC.Group/Model/Nivel.cs
+++ TGC.Group/Model/Nivel.cs
@@ -34,6 +34,8 @@ namespace TGC.Group.Model {
             var paredJunglaTexture = TgcTexture.createTexture(D3DDevice.Instance.Device, mediaDir + "paredJungla.jpg");
             var desiertoTexture = TgcTexture.createTexture(D3DDevice.Instance.Device, mediaDir + "arena.jpg");
             var piedraTexture = TgcTexture.createTexture(D3DDevice.Instance.Device, mediaDir + "piedra.png");
+            var precipicioTexture = TgcTexture.createTexture(D3DDevice.Instance.Device, mediaDir + "precipicio.jpg");
+            var maderaTexture = TgcTexture.createTexture(D3DDevice.Instance.Device, mediaDir + "tronco.jpg");
 
             var piso = new TgcPlane(new TGCVector3(-500, 0, -500), new TGCVector3(2500, 0, 2500), TgcPlane.Orientations.XZplane, pisoTexture);
             pisosNormales.Add(piso); //piso de la jungla
@@ -41,7 +43,7 @@ namespace TGC.Group.Model {
             piso = new TgcPlane(new TGCVector3(-500, -180, 2600), new TGCVector3(1000, 0, 3500), TgcPlane.Orientations.XZplane, desiertoTexture);
             pisosNormales.Add(piso); // piso del desierto
 
-            piso = new TgcPlane(new TGCVector3(-500, 0, -2500), new TGCVector3(2500, 0, 2000), TgcPlane.Orientations.XZplane, hieloTexture);
+            piso = new TgcPlane(new TGCVector3(-500, 0, -3000), new TGCVector3(2500, 0, 2500), TgcPlane.Orientations.XZplane, hieloTexture);
             pisosResbaladizos.Add(piso); // piso de hielo
 
             /*
@@ -52,10 +54,12 @@ namespace TGC.Group.Model {
             */
 
             // paredes de la jungla
-            pEstaticas.Add(new Plataforma(new TGCVector3(-500, 150, 500), new TGCVector3(100, 300, 3000), paredJunglaTexture));
-            pEstaticas.Add(new Plataforma(new TGCVector3(500, 150, 500), new TGCVector3(100, 300, 3000), paredJunglaTexture));
-            
-            //paredes del desierto
+            pEstaticas.Add(new Plataforma(new TGCVector3(-500, 150, 600), new TGCVector3(100, 300, 2800), paredJunglaTexture)); //laterales jungla derecha
+            pEstaticas.Add(new Plataforma(new TGCVector3(500, 150, 600), new TGCVector3(100, 300, 2800), paredJunglaTexture));
+            pEstaticas.Add(new Plataforma(new TGCVector3(1975, 150, 0), new TGCVector3(50, 300, 1600), paredJunglaTexture)); // borde izquierdo jungla derecha
+            pEstaticas.Add(new Plataforma(new TGCVector3(1250, 30, 1990), new TGCVector3(1500, 60, 20), paredJunglaTexture)); // fondo jungla izquierda
+
+            //paredes del desierto; el desierto está a un nivel inferior que la jungla y los glaciares
             pEstaticas.Add(new Plataforma(new TGCVector3(500, -150, 4350), new TGCVector3(100, 60, 3500), desiertoTexture));
             pEstaticas.Add(new Plataforma(new TGCVector3(-500, -150, 4350), new TGCVector3(100, 60, 3500), desiertoTexture));
             pEstaticas.Add(new Plataforma(new TGCVector3(0, -165, 6090), new TGCVector3(900, 70, 20), desiertoTexture));
@@ -72,7 +76,22 @@ namespace TGC.Group.Model {
             pEstaticas.Add(new Plataforma(new TGCVector3(500, -80, 2100), new TGCVector3(100, 200, 200), piedraTexture));  // contornos superior
             pEstaticas.Add(new Plataforma(new TGCVector3(-500, -80, 2100), new TGCVector3(100, 200, 200), piedraTexture));
 
+            //paredes de los glaciares
+            pEstaticas.Add(new Plataforma(new TGCVector3(-500, 200, -1900), new TGCVector3(100, 400, 2200), hieloTexture)); // derecha
+            pEstaticas.Add(new Plataforma(new TGCVector3(750, 200, -2510), new TGCVector3(2500, 400, 20), hieloTexture));  // fondo
+            pEstaticas.Add(new Plataforma(new TGCVector3(1975, 200, -1900), new TGCVector3(50, 400, 2200), hieloTexture)); // izquierda
+
+            // precipicio del tronco
+            piso = new TgcPlane(new TGCVector3(2000, -200, 800), new TGCVector3(1000, 0, 1200), TgcPlane.Orientations.XZplane, precipicioTexture);
+            pisosNormales.Add(piso); //TODO: Configurar el deathplane, deberia ir ahi?
+            pEstaticas.Add(new Plataforma(new TGCVector3(2500, -100, 800), new TGCVector3(1000, 200, 2), precipicioTexture)); // fondo
+            pEstaticas.Add(new Plataforma(new TGCVector3(2000, -100, 1400), new TGCVector3(2, 200, 1200), precipicioTexture)); // derecha
+            pEstaticas.Add(new Plataforma(new TGCVector3(3000, -100, 1400), new TGCVector3(2, 200, 1200), precipicioTexture)); // izquierda
+            pEstaticas.Add(new Plataforma(new TGCVector3(2500, -100, 2000), new TGCVector3(1000, 200, 2), precipicioTexture)); // frontal
+
             pDesplazan.Add(new PlataformaDesplazante(new TGCVector3(300, 120, 300), new TGCVector3(200, 50, 200), cajaTexture, new TGCVector3(-300, 100, -300), new TGCVector3(0.5f, 0, 0.5f)));
+            // tronco que se desplaza en el precipicio; TODO: Revisar movimiento en conjunto con tgcito
+            pDesplazan.Add(new PlataformaDesplazante(new TGCVector3(2075, -60, 1400), new TGCVector3(150, 50, 80), maderaTexture, new TGCVector3(2925, -60, 1400), new TGCVector3(0.2f, 0, 0)));
 
             pRotantes.Add(new PlataformaRotante(new TGCVector3(0, 100, 300), new TGCVector3(200, 50, 200), cajaTexture, FastMath.QUARTER_PI/4));
         }
