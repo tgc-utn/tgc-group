3335e45bc1d10048248a2cc8b4a547e984231c04
Beto <lopezalbertogabriel@gmail.com>
2018-05-08 10:05:11 -0300

    agrego por codigo los elementos del XML

--- TGC.Group/Model/GameModel.cs
+++ TGC.Group/Model/GameModel.cs
@@ -32,12 +32,7 @@ namespace TGC.Group.Model
         private TgcScene scene;
         private TgcText2D textoVelocidadVehiculo, textoOffsetH, textoOffsetF, textoPosicionVehiculo, textoVectorAdelante, textoVectorCostado;
         private TgcMesh jabon;
-        private TgcMesh cama;
-        private TgcMesh mesaDeLuz;
-        private TgcMesh ropero;
-        private TgcMesh armario;
-        private TgcMesh escritorio;
-        private TgcMesh sillaEscritorio;
+        private TgcMesh cama, mesaDeLuz, ropero, armario, escritorio, sillaEscritorio, dispenser, mesa, heladera, tacho, sillaCocina, muebleCocina;
 
         private List<TgcMesh> losMeshes = new List<TgcMesh>();
 
@@ -51,6 +46,7 @@ namespace TGC.Group.Model
             {
                 losMeshes.Add(mesh);
             }
+
             this.jabon = this.dameMesh("MeshCreator\\Meshes\\Bathroom\\Jabon\\Jabon-TgcScene.xml", new TGCVector3(1, 1, 1), new TGCVector3(0, 0, 0), new TGCVector3(100f, 0f, 10f));
             this.cama = this.dameMesh("MeshCreator\\Meshes\\Habitacion\\Cama\\Cama-TgcScene.xml", new TGCVector3(1,1,1), new TGCVector3(0, FastMath.PI, 0), new TGCVector3(-36f, 0, -124f));
 
@@ -59,6 +55,14 @@ namespace TGC.Group.Model
             this.armario = this.dameMesh("MeshCreator\\Meshes\\Habitacion\\Armario\\Armario-TgcScene.xml", new TGCVector3(1,1,1), new TGCVector3(0,-FastMath.PI_HALF,0), new TGCVector3(-30f,0,110f));
             this.escritorio = this.dameMesh("MeshCreator\\Meshes\\Habitacion\\Escritorio\\Escritorio-TgcScene.xml", new TGCVector3(1, 1, 1), new TGCVector3(0, 0, 0), new TGCVector3(183f,0f,-107f));
             this.sillaEscritorio = this.dameMesh("MeshCreator\\Meshes\\Habitacion\\SillaEscritorio\\SillaEscritorio-TgcScene.xml", new TGCVector3(0.5f, 0.3f, 0.5f),new TGCVector3(0,0,0), new TGCVector3(151f, -1f, -101f));
+            this.dispenser = this.dameMesh("MeshCreator\\Meshes\\Cocina\\DispenserAgua\\DispenserAgua-TgcScene.xml", new TGCVector3(1f, 1f, 1f),new TGCVector3(0,0,0), new TGCVector3(-35f, -1f, 360f));
+            this.mesa = this.dameMesh("MeshCreator\\Meshes\\Cocina\\Mesa\\Mesa-TgcScene.xml", new TGCVector3(1f, 1f, 1f),new TGCVector3(0,FastMath.PI_HALF,0), new TGCVector3(100f, -1f, 340f));
+            this.heladera = this.dameMesh("MeshCreator\\Meshes\\Cocina\\Heladera\\Heladera-TgcScene.xml", new TGCVector3(1f, 1f, 1f),new TGCVector3(0,0,0), new TGCVector3(190f, -1f, 180f));
+            this.tacho = this.dameMesh("MeshCreator\\Meshes\\Cocina\\Tacho\\Tacho-TgcScene.xml", new TGCVector3(0.6f, 0.6f, 0.6f),new TGCVector3(0,0,0), new TGCVector3(-30f, -1f, 165f));
+            this.sillaCocina = this.dameMesh("MeshCreator\\Meshes\\Cocina\\Silla\\silla-TgcScene.xml", new TGCVector3(0.5f, 0.5f, 0.5f),new TGCVector3(0,FastMath.QUARTER_PI,0), new TGCVector3(100f, -1f, 310f));
+            this.muebleCocina = this.dameMesh("MeshCreator\\Meshes\\Cocina\\Mueble\\Mueble-TgcScene.xml", new TGCVector3(1f, 1f, 0.5f),new TGCVector3(0,0,0), new TGCVector3(10f, -1f, 360f));
+         
+
             //creo el vehiculo liviano
             //si quiero crear un vehiculo pesado (camion) hago esto
             // VehiculoPesado camion = new VehiculoPesado(rutaAMesh);
@@ -285,14 +289,21 @@ namespace TGC.Group.Model
 
         private TgcMesh dameMesh(string ruta, TGCVector3 escala, TGCVector3 rotacion, TGCVector3 traslado)
         {
-            var mesh = new TgcSceneLoader().loadSceneFromFile(MediaDir + ruta).Meshes[0];
+            TgcScene tgcScene = new TgcSceneLoader().loadSceneFromFile(MediaDir + ruta);
+
+            foreach(var mesh in tgcScene.Meshes)
+            {
+
             mesh.Scale = escala;
             mesh.RotateX(rotacion.X);
             mesh.RotateY(rotacion.Y);
             mesh.RotateZ(rotacion.Z);
             mesh.Move(traslado);
             losMeshes.Add(mesh);
-            return mesh;
+            }
+
+            var meshZero = tgcScene.Meshes[0];
+            return meshZero;
         }
     }
 }
