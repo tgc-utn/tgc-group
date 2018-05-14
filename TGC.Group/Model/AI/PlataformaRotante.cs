99e10d5bdeabf0d8eff5439e96ef32c89ad39414
icasarino <nachocas2010@gmail.com>
2018-05-14 00:01:08 -0300

    Rotación Mesh

--- TGC.Group/Model/AI/PlataformaRotante.cs
+++ TGC.Group/Model/AI/PlataformaRotante.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using TGC.Core.Mathematica;
 using TGC.Core.SceneLoader;
 using TGC.Core.Collision;
-
+using TGC.Core.BoundingVolumes;
 namespace TGC.Group.Model.AI
 {
     class PlataformaRotante : Plataforma
@@ -14,29 +14,34 @@ namespace TGC.Group.Model.AI
         private TgcMesh plataformaMesh;
         private Escenario escenario;
         public float anguloRotacion;
-        //private TGCVector3 pivote;
-        private TGCVector3 initPosMesh;
+
         private TGCVector3 initPosBB;
+        private TGCMatrix MTraslacionOrigen;
+        private TGCMatrix MTraslacionPos;
+        
         public PlataformaRotante(TgcMesh plataformaMesh, Escenario escenario) : base(plataformaMesh, escenario)
         {
             this.plataformaMesh = plataformaMesh;
             this.escenario = escenario;
             this.plataformaMesh.AutoTransform = false;
-            this.plataformaMesh.AutoUpdateBoundingBox = true;
+            this.plataformaMesh.AutoUpdateBoundingBox = false;
             anguloRotacion = FastMath.ToRad(10f);
-            initPosMesh = this.plataformaMesh.Position; /* Posición Inicial Mesh de la plataforma. 
-                                                         * Por alguna razón, desde la inicialización se setea en (0,0,0)
-                                                         */
+
             initPosBB = this.plataformaMesh.BoundingBox.calculateBoxCenter(); //Posicion inicial BoundingBox (Se setea en la posición correcta)
-    }
+            MTraslacionPos = TGCMatrix.Translation(initPosBB);
+             
+            MTraslacionOrigen = TGCMatrix.Translation(new TGCVector3(-initPosBB.X, -initPosBB.Y, -initPosBB.Z));
+
+
+           // var a = new TgcBoundingOrientedBox(); --> Adaptar y usar como  nueva BoundingBox
+           
+        }
         
         public override void Update(float tiempo)
         {
-            var TMesh = TGCMatrix.RotationAxis(initPosBB,anguloRotacion * tiempo);
-            //var TBB = TGCMatrix.RotationAxis(initPosMesh, anguloRotacion * tiempo);
-            plataformaMesh.Transform = TMesh;
-           // plataformaMesh.BoundingBox.transform(TGCMatrix.RotationAxis(initPosBB,anguloRotacion *tiempo));
-
+            var Rot = MTraslacionOrigen * TGCMatrix.RotationY(anguloRotacion * tiempo) * MTraslacionPos;
+            plataformaMesh.Transform = Rot;
+            
         }
     }
 }
