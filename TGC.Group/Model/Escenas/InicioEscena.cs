baaf820dae82eca64b2adc0a748b7fa33c7aa0fa
soriac <sbcristiansoria@gmail.com>
2018-05-28 16:56:39 -0300

    Elimino pre/post render

---
+++ TGC.Group/Model/Escenas/InicioEscena.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TGC.Core.Camara;
+using TGC.Core.Input;
+
+namespace TGC.Group.Model.Escenas {
+    class InicioEscena : Escena {
+        public void init(string mediaDir) {
+
+        }
+
+        public void render(float deltaTime) {
+
+        }
+
+        public void update(float deltaTime, TgcD3dInput input, TgcCamera camara) {
+            
+        }
+
+        public void dispose() {
+
+        }
+
+    }
+}
--- TGC.Group/Model/GameModel.cs
+++ TGC.Group/Model/GameModel.cs
@@ -1,4 +1,9 @@
+using Microsoft.DirectX.Direct3D;
+using System.Drawing;
+using TGC.Core.Direct3D;
 using TGC.Core.Example;
+using TGC.Core.Textures;
+using TGC.Group.Model.Escenas;
 using TGC.Group.Model.Scenes;
 
 namespace TGC.Group.Model {
@@ -17,14 +22,25 @@ namespace TGC.Group.Model {
 
         public override void Update() {
             PreUpdate();
+
             EscenaManager.getInstance().update(ElapsedTime, Input, Camara);
+
             PostUpdate();
         }
 
         public override void Render() {
-            PreRender();
+            // prerender
+            // TODO: ver el color/poner skybox
+            D3DDevice.Instance.Device.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.CadetBlue, 1.0f, 0);
+            D3DDevice.Instance.Device.BeginScene();
+            TexturesManager.Instance.clearAll();
+
             EscenaManager.getInstance().render(ElapsedTime);
-            PostRender();
+
+            RenderAxis();
+            RenderFPS();
+            D3DDevice.Instance.Device.EndScene();
+            D3DDevice.Instance.Device.Present();
         }
 
         public override void Dispose() {
