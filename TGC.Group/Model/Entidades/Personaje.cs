d623c6aef9f2f41af810e100a7b7bec603daaf87
soriac <sbcristiansoria@gmail.com>
2018-05-29 16:29:17 -0300

    Sistema básico de stamina.

--- TGC.Group/Model/Entidades/Personaje.cs
+++ TGC.Group/Model/Entidades/Personaje.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.DirectX.DirectInput;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using TGC.Core.BoundingVolumes;
 using TGC.Core.Collision;
@@ -21,6 +22,9 @@ namespace TGC.Group.Model {
         private TGCVector3 POS_ORIGEN = new TGCVector3(0, 100, 9500);
 
         private int vidas;
+        private int stamina;
+        private bool agotado;
+        private const int MAX_STAMINA = 200;
 
         // animaciones
         private TgcSkeletalMesh mesh;
@@ -35,7 +39,7 @@ namespace TGC.Group.Model {
         private TGCVector3 dir;
         private TGCVector3 vel;
         private const float WALK_SPEED = 500f;
-        private const float MULT_CORRER = 1.5f;
+        private const float MULT_CORRER = 1.75f;
         private const float MULT_CAMINAR = 0.5f;
         private const float VEL_TERMINAL = -10;
         private const float MODIFICADOR_HIELO = 0.75f;
@@ -47,10 +51,12 @@ namespace TGC.Group.Model {
         private const float JUMP_SPEED = 10f;
 
         public Personaje(string MediaDir) {
+            vidas = 3;
+            stamina = MAX_STAMINA;
+
             dir = TGCVector3.Empty;
             vel = TGCVector3.Empty;
             var skeletalLoader = new TgcSkeletalLoader();
-
 
             mesh = skeletalLoader.loadMeshAndAnimationsFromFile(
                 MediaDir + "Robot\\Robot-TgcSkeletalMesh.xml",
@@ -77,7 +83,6 @@ namespace TGC.Group.Model {
             posicionPies.Y = mesh.BoundingBox.PMin.Y;
 
             pies = new TgcBoundingSphere(posicionPies, 10);
-            vidas = 3;
 
             sombraTexture = TgcTexture.createTexture(MediaDir + "sombra.png");
             sombra = TGCBox.fromSize(posicionPies, new TGCVector3(100, 1, 100), sombraTexture);
@@ -100,7 +105,8 @@ namespace TGC.Group.Model {
             pies.Render();
 
             TgcText2D t = new TgcText2D();
-            t.Text = vidas.ToString();
+            t.Text = stamina.ToString();
+            t.Color = Color.White;
             t.render();
 
             // seria un post-update
@@ -145,12 +151,22 @@ namespace TGC.Group.Model {
                 saltosRestantes--;
             }
 
-            if (Input.keyDown(Key.LeftShift)) {
+            if (Input.keyDown(Key.LeftShift) && !agotado) {
                 dir.X = dir.X * MULT_CORRER;
                 dir.Z = dir.Z * MULT_CORRER;
+                stamina--;
+                if (stamina == 0) agotado = true;
             } else if (Input.keyDown(Key.LeftAlt)) {
                 dir.X = dir.X * MULT_CAMINAR;
                 dir.Z = dir.Z * MULT_CAMINAR;
+                stamina += 2;
+            } else {
+                stamina++;
+            }
+
+            if (stamina > MAX_STAMINA) {
+                stamina = MAX_STAMINA;
+                agotado = false;
             }
         }
 
