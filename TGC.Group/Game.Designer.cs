af5e76295a972824b405919c5fa918775dc60416
Manuel Tuero <manutuero@gmail.com>
2018-09-10 13:13:47 -0300

    Refactor of GameModel main class.

--- TGC.Group/Game.Designer.cs
+++ TGC.Group/Game.Designer.cs
@@ -12,7 +12,7 @@ namespace TGC.Group {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
     internal sealed partial class Game : global::System.Configuration.ApplicationSettingsBase {
         
         private static Game defaultInstance = ((Game)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Game())));
@@ -37,7 +37,7 @@ namespace TGC.Group {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Grupo 99")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Grupo 1")]
         public string Name {
             get {
                 return ((string)(this["Name"]));
@@ -49,7 +49,7 @@ namespace TGC.Group {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Mi idea - Descripción de la idea")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Crash Bandicoot - Juego de plataformas en 3D.")]
         public string Description {
             get {
                 return ((string)(this["Description"]));
--- TGC.Group/Model/GameModel.cs
+++ TGC.Group/Model/GameModel.cs
@@ -1,21 +1,19 @@
 using Microsoft.DirectX.DirectInput;
-using System;
 using System.Drawing;
 using TGC.Core.Direct3D;
 using TGC.Core.Example;
-using TGC.Core.Geometry;
 using TGC.Core.Input;
 using TGC.Core.Mathematica;
 using TGC.Core.SceneLoader;
-using TGC.Core.Textures;
 using TGC.Core.Terrain;
 
 namespace TGC.Group.Model
 {
     /// <summary>
     ///     Ejemplo para implementar el TP.
-    ///     Inicialmente puede ser renombrado o copiado para hacer más ejemplos chicos, en el caso de copiar para que se
-    ///     ejecute el nuevo ejemplo deben cambiar el modelo que instancia GameForm <see cref="Form.GameForm.InitGraphics()" />
+    ///     Inicialmente puede ser renombrado o copiado para hacer más ejemplos chicos, 
+    ///     en el caso de copiar para que se ejecute el nuevo ejemplo deben cambiar el modelo 
+    ///     que instancia GameForm <see cref="Form.GameForm.InitGraphics()" />
     ///     line 97.
     /// </summary>
     public class GameModel : TgcExample
@@ -26,10 +24,17 @@ namespace TGC.Group.Model
         /// <param name="mediaDir">Ruta donde esta la carpeta con los assets</param>
         /// <param name="shadersDir">Ruta donde esta la carpeta con los shaders</param>
 
+        /* Estos 4 atributos no dseberian estar en la Clase GameModel, refactorizar!! */ 
         private bool saltando;
         private int direccionSalto = 1;
         private float posInicialBandicoot;
         private float alturaMaximaSalto = 20f;
+
+        private const float MOVEMENT_SPEED = 100f;
+        private TgcMesh Bandicoot { get; set; } 
+        private bool BoundingBox { get; set; }
+        private TgcSimpleTerrain terrain;
+
         public GameModel(string mediaDir, string shadersDir) : base(mediaDir, shadersDir)
         {
             Category = Game.Default.Category;
@@ -37,85 +42,73 @@ namespace TGC.Group.Model
             Description = Game.Default.Description;
         }
 
-        private const float MOVEMENT_SPEED = 100f;
-        //private TgcThirdPersonCamera camaraInterna;
-       
-        //Caja que se muestra en el ejemplo.
-        private TGCBox Suelo { get; set; }
+        public void InitTerrain()
+        {
+            string heightmapPath = $"{MediaDir}\\Heightmap\\hawai.jpg";
+            string texturePath = $"{MediaDir}\\Textures\\TerrainTextureHawaii.jpg";
+            var center = new TGCVector3(0f, 0f, 0f);
+            float scaleXZ = 50f;
+            float scaleY = 5f;
 
-        //Mesh de Bandicoot.
-        private TgcMesh Bandicoot { get; set; }
+            terrain = new TgcSimpleTerrain();
+            terrain.loadHeightmap(heightmapPath, scaleXZ, scaleY, center);
+            terrain.loadTexture(texturePath);
+        }
 
-        //Boleano para ver si dibujamos el boundingbox
-        private bool BoundingBox { get; set; }
+        public void InitMeshes()
+        {
+            var sceneLoader = new TgcSceneLoader();
+            string path = $"{MediaDir}/crash/CRASH (2)-TgcScene.xml";
+            var pMin = new TGCVector3(0, 0, 0);
+            var pMax = new TGCVector3(-185f, 225f, -100f);
 
-        private TgcSimpleTerrain terrain;
+            Bandicoot = sceneLoader.loadSceneFromFile(path).Meshes[0];
+            Bandicoot.Scale = new TGCVector3(0.05f, 0.05f, 0.05f);
+            Bandicoot.RotateY(3.12f);
+            Bandicoot.BoundingBox.setExtremes(pMin, pMax);
+        }
+
+        public void InitCamera()
+        {
+            /* Suelen utilizarse objetos que manejan el comportamiento de la camara.
+               Lo que en realidad necesitamos gráficamente es una matriz de View.
+               El framework maneja una cámara estática, pero debe ser inicializada.
+               Internamente el framework construye la matriz de view con estos dos vectores.
+               Luego en nuestro juego tendremos que crear una cámara que cambie 
+               la matriz de view con variables como movimientos o animaciones de escenas. */
+            var postition = new TGCVector3(-5, 20, 50);
+            var lookAt = Bandicoot.Position;
+
+            Camara.SetCamera(postition, lookAt);
+        }
+        
 
         /// <summary>
-        ///     Se llama una sola vez, al principio cuando se ejecuta el ejemplo.
-        ///     Escribir aquí todo el código de inicialización: cargar modelos, texturas, estructuras de optimización, todo
-        ///     procesamiento que podemos pre calcular para nuestro juego.
-        ///     Borrar el codigo ejemplo no utilizado.
+        ///     Escribir aquí todo el código de inicialización: cargar modelos, texturas, 
+        ///     estructuras de optimización y todo procesamiento que podemos pre calcular para nuestro juego.
         /// </summary>
         public override void Init()
         {
             //Device de DirectX para crear primitivas.
             var d3dDevice = D3DDevice.Instance.Device;
 
-            var path = $"{MediaDir}\\Heightmap\\hawai.jpg";
-            var center = new TGCVector3(0f, 0f, 0f);
-            terrain = new TgcSimpleTerrain();
-            terrain.loadHeightmap(path, 20f, 1.3f, center);
+            InitTerrain();
+            InitMeshes();
+            InitCamera();
 
-            //Textura de la carperta Media. Game.Default es un archivo de configuracion (Game.settings) util para poner cosas.
-            //Pueden abrir el Game.settings que se ubica dentro de nuestro proyecto para configurar.
-            var TexturaSuelo = MediaDir + "/floor2_3.jpg";
-
-            //Cargamos una textura, tener en cuenta que cargar una textura significa crear una copia en memoria.
-            //Es importante cargar texturas en Init, si se hace en el render loop podemos tener grandes problemas si instanciamos muchas.
-            var texture = TgcTexture.createTexture(TexturaSuelo);
-
-            //Creamos una caja 3D ubicada de dimensiones (5, 10, 5) y la textura como color.
-            var size = new TGCVector3(80, 1, 300);
-            //Construimos una caja según los parámetros, por defecto la misma se crea con centro en el origen y se recomienda así para facilitar las transformaciones.
-            Suelo = TGCBox.fromSize(size, texture);
-            //Posición donde quiero que este la caja, es común que se utilicen estructuras internas para las transformaciones.
-            //Entonces actualizamos la posición lógica, luego podemos utilizar esto en render para posicionar donde corresponda con transformaciones.
-            Suelo.Position = new TGCVector3(0, -.5f, -140);
-
-            //Cargo el unico mesh que tiene la escena.
-            Bandicoot = new TgcSceneLoader().loadSceneFromFile(MediaDir + "/crash/CRASH (2)-TgcScene.xml").Meshes[0];
-            //Defino una escala en el modelo logico del mesh que es muy grande.
-            Bandicoot.Scale = new TGCVector3(0.05f, 0.05f, 0.05f);
-            Bandicoot.RotateY(3.12f);
-
-            //Suelen utilizarse objetos que manejan el comportamiento de la camara.
-            //Lo que en realidad necesitamos gráficamente es una matriz de View.
-            //El framework maneja una cámara estática, pero debe ser inicializada.
-            //Posición de la camara.
-            var cameraPosition = new TGCVector3(-5, 20, 50);
-            //Quiero que la camara mire hacia el origen (0,0,0).
-            var lookAt = Bandicoot.Position;
-            //Configuro donde esta la posicion de la camara y hacia donde mira.
-
-            Camara.SetCamera(cameraPosition, lookAt);
-            //Internamente el framework construye la matriz de view con estos dos vectores.
-            //Luego en nuestro juego tendremos que crear una cámara que cambie la matriz de view con variables como movimientos o animaciones de escenas.
-            Bandicoot.BoundingBox.setExtremes(new TGCVector3(0, 0, 0), new TGCVector3(-185f, 225f, -100f));
             posInicialBandicoot = Bandicoot.Position.Y;
         }
 
         /// <summary>
         ///     Se llama en cada frame.
-        ///     Se debe escribir toda la lógica de computo del modelo, así como también verificar entradas del usuario y reacciones
-        ///     ante ellas.
+        ///     Se debe escribir toda la lógica de computo del modelo,
+        ///     así como también verificar entradas del usuario y reacciones ante ellas.
         /// </summary>
         public override void Update()
         {
             PreUpdate();
 
-            //Capturar Input teclado utilizado para movimiento 
-
+            // Capturar Input teclado utilizado para movimiento 
             var anguloCamara = TGCVector3.Empty;
             var movimiento = TGCVector3.Empty;
 
@@ -145,8 +138,7 @@ namespace TGC.Group.Model
             }
 
             //salto
-
-            if(Input.keyPressed(Key.Space) && !saltando)
+            if (Input.keyPressed(Key.Space) && !saltando)
             {
                 saltando = true;
                 direccionSalto = 1;
@@ -160,23 +152,23 @@ namespace TGC.Group.Model
             movimiento *= MOVEMENT_SPEED * ElapsedTime;
 
             Bandicoot.Move(movimiento);
-            if (saltando){
-                Bandicoot.Move(0,direccionSalto* MOVEMENT_SPEED * ElapsedTime, 0);
+            if (saltando)
+            {
+                Bandicoot.Move(0, direccionSalto * MOVEMENT_SPEED * ElapsedTime, 0);
 
                 //Si la posicion en Y es mayor a la maxima altura. 
-                if (Bandicoot.Position.Y > alturaMaximaSalto) {
+                if (Bandicoot.Position.Y > alturaMaximaSalto)
+                {
                     direccionSalto = -1;
                 }
 
-                if (Bandicoot.Position.Y <= posInicialBandicoot) {
+                if (Bandicoot.Position.Y <= posInicialBandicoot)
+                {
                     saltando = false;
                 }
             }
 
-
-
             //Desplazar camara para seguir al personaje
-
             Camara.SetCamera(Camara.Position + new TGCVector3(movimiento), anguloCamara);
 
             //Capturar Input Mouse
@@ -209,7 +201,6 @@ namespace TGC.Group.Model
             PostUpdate();
         }
 
-   
         /// <summary>
         ///     Se llama cada vez que hay que refrescar la pantalla.
         ///     Escribir aquí todo el código referido al renderizado.
@@ -226,42 +217,28 @@ namespace TGC.Group.Model
 
             terrain.Render();
 
-            //Siempre antes de renderizar el modelo necesitamos actualizar la matriz de transformacion.
-            //Debemos recordar el orden en cual debemos multiplicar las matrices, en caso de tener modelos jerárquicos, tenemos control total.
-            Suelo.Transform = TGCMatrix.Scaling(Suelo.Scale) * TGCMatrix.RotationYawPitchRoll(Suelo.Rotation.Y, Suelo.Rotation.X, Suelo.Rotation.Z) * TGCMatrix.Translation(Suelo.Position);
-            //A modo ejemplo realizamos toda las multiplicaciones, pero aquí solo nos hacia falta la traslación.
-            //Finalmente invocamos al render de la caja
-            Suelo.Render();
-
-            //Cuando tenemos modelos mesh podemos utilizar un método que hace la matriz de transformación estándar.
-            //Es útil cuando tenemos transformaciones simples, pero OJO cuando tenemos transformaciones jerárquicas o complicadas.
+            // Cuando tenemos modelos mesh podemos utilizar un método que hace la matriz de transformación estándar.
+            // Es útil cuando tenemos transformaciones simples, pero OJO cuando tenemos transformaciones jerárquicas o complicadas.
             Bandicoot.UpdateMeshTransform();
-            //Render del mesh
             Bandicoot.Render();
 
-            //Render de BoundingBox, muy útil para debug de colisiones.
             if (BoundingBox)
             {
-                Suelo.BoundingBox.Render();
                 Bandicoot.BoundingBox.Render();
             }
 
-            //Finaliza el render y presenta en pantalla, al igual que el preRender se debe para casos puntuales es mejor utilizar a mano las operaciones de EndScene y PresentScene
+            // Finaliza el render y presenta en pantalla, al igual que el preRender se debe usar para casos 
+            // puntuales es mejor utilizar a mano las operaciones de EndScene y PresentScene
             PostRender();
         }
 
         /// <summary>
-        ///     Se llama cuando termina la ejecución del ejemplo.
-        ///     Hacer Dispose() de todos los objetos creados.
-        ///     Es muy importante liberar los recursos, sobretodo los gráficos ya que quedan bloqueados en el device de video.
+        ///     Es muy importante liberar los recursos, sobretodo los gráficos
+        ///     ya que quedan bloqueados en el device de video.
         /// </summary>
         public override void Dispose()
         {
-            //Dispose de la caja.
-            Suelo.Dispose();
-            //Dispose del mesh.
             Bandicoot.Dispose();
-
             terrain.Dispose();
         }
     }
