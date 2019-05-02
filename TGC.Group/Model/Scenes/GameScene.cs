2249d65ded6055c6387fdcf84952521154618f7f
Juan Amarillo <juan.alb.amarillo@gmail.com>
2019-05-02 03:59:53 -0300

    removed unnecessary code

--- TGC.Group/Model/Scenes/GameScene.cs
+++ TGC.Group/Model/Scenes/GameScene.cs
@@ -31,35 +31,8 @@ namespace TGC.Group.Model.Scenes
         { 
             PhysicsWorld.Init();
             backgroundColor = Color.FromArgb(1, 78, 129, 179);
-
-            this.World = new World(new TGCVector3(0, 0, 0));
-            
-            //Device de DirectX para crear primitivas.
-            var d3dDevice = D3DDevice.Instance.Device;
-
-            //Textura de la carperta Media. Game.Default es un archivo de configuracion (Game.settings) util para poner cosas.
-            //Pueden abrir el Game.settings que se ubica dentro de nuestro proyecto para configurar.
-            var pathTexturaCaja = mediaDir + Game.Default.TexturaCaja;
-
-            //Cargamos una textura, tener en cuenta que cargar una textura significa crear una copia en memoria.
-            //Es importante cargar texturas en Init, si se hace en el render loop podemos tener grandes problemas si instanciamos muchas.
-            var texture = TgcTexture.createTexture(pathTexturaCaja);
-
-            //Creamos una caja 3D ubicada de dimensiones (5, 10, 5) y la textura como color.
-            var size = new TGCVector3(5, 10, 5);
-            //Construimos una caja según los parámetros, por defecto la misma se crea con centro en el origen y se recomienda así para facilitar las transformaciones.
-            //Posición donde quiero que este la caja, es común que se utilicen estructuras internas para las transformaciones.
-            //Entonces actualizamos la posición lógica, luego podemos utilizar esto en render para posicionar donde corresponda con transformaciones.
-           // Box = new Element(new TGCVector3(-25, 0, 0), TGCBox.fromSize(size, texture).ToMesh("caja"));
-
-            //Cargo el unico mesh que tiene la escena.
-            TgcMesh Mesh = new TgcSceneLoader().loadSceneFromFile(mediaDir + "LogoTGC-TgcScene.xml").Meshes[0];
-            //Defino una escala en el modelo logico del mesh que es muy grande.
-            Mesh.Scale = new TGCVector3(0.5f, 0.5f, 0.5f);
-
-            //this.TgcLogo = new Element(TGCVector3.Empty, Mesh);
-            this.Camera = new Camera(new TGCVector3(30, 30, 200), input);
-
+            World = new World(new TGCVector3(0, 0, 0));
+            Camera = new Camera(new TGCVector3(30, 30, 200), input);
         }
 
         public override void Update(float elapsedTime)
@@ -85,11 +58,8 @@ namespace TGC.Group.Model.Scenes
             this.DrawText.drawText("Con la tecla F se dibuja el bounding box.", 0, 20, Color.OrangeRed);
             this.DrawText.drawText("Con clic izquierdo subimos la camara [Actual]: " + TGCVector3.PrintVector3(this.Camera.Position), 0, 30, Color.OrangeRed);
 
-            //this.Box.Render();
             this.World.Render(this.Camera.Position);
 
-
-            //Render de BoundingBox, muy útil para debug de colisiones.
             if (this.BoundingBox) {
                 this.World.RenderBoundingBox(this.Camera.Position);
             }
@@ -97,7 +67,6 @@ namespace TGC.Group.Model.Scenes
 
         public override void Dispose()
         {
-            //this.Box.Dispose();
             this.World.Dispose();
         }
 
