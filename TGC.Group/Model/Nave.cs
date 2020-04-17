2af841603c36c4bbff034c8486d1a2f88bc1ccd7
tfloxolodeiro <tfloxolodeiro@est.frba.utn.edu.ar>
2020-04-17 11:48:08 -0300

    Revertida una linea de prueba

--- TGC.Group/Model/Nave.cs
+++ TGC.Group/Model/Nave.cs
@@ -51,7 +51,6 @@ namespace TGC.Group.Model
 
             mainMesh.Position += movimientoDelFrame;
             mainMesh.Transform = TGCMatrix.Translation(mainMesh.Position);
-            mainMesh.Transform = TGCMatrix.RotationY(9.0f);
 
         }
 
