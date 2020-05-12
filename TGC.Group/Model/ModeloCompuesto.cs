9fcb2f08544f670997066f7541e508beeade516c
tfloxolodeiro <tfloxolodeiro@est.frba.utn.edu.ar>
2020-05-11 22:09:43 -0300

    Fix de desastres

--- TGC.Group/Model/ModeloCompuesto.cs
+++ TGC.Group/Model/ModeloCompuesto.cs
@@ -9,11 +9,12 @@ using TGC.Core.SceneLoader;
 
 namespace TGC.Group.Model
 {
-    
+
     class ModeloCompuesto
     {
         private readonly List<TgcMesh> meshes;
-
+        private TGCMatrix baseScaleRotation;
+        private TGCMatrix rotacionMesh;
         public ModeloCompuesto(string direccionDelModelo, TGCVector3 posicionInicial)
         {
             meshes = new TgcSceneLoader().loadSceneFromFile(direccionDelModelo).Meshes;
@@ -34,19 +35,26 @@ namespace TGC.Group.Model
 
         public void CambiarRotacion(TGCVector3 nuevaRotacion)
         {
-            /*
+
             TGCQuaternion rotationX = TGCQuaternion.RotationAxis(new TGCVector3(1.0f, 0.0f, 0.0f), nuevaRotacion.X);
             TGCQuaternion rotationY = TGCQuaternion.RotationAxis(new TGCVector3(0.0f, 1.0f, 0.0f), nuevaRotacion.Y);
             TGCQuaternion rotationZ = TGCQuaternion.RotationAxis(new TGCVector3(0.0f, 0.0f, 1.0f), nuevaRotacion.Z);
 
             TGCQuaternion rotation = rotationX * rotationY * rotationZ;
 
-            TGCMatrix baseScaleRotation = TGCMatrix.Scaling(new TGCVector3(0.05f, 0.05f, 0.05f)) * TGCMatrix.RotationY(FastMath.PI_HALF);
+            baseScaleRotation = TGCMatrix.Scaling(new TGCVector3(0.15f, 0.15f, 0.15f)) * TGCMatrix.RotationY(FastMath.PI_HALF);
 
-            TransformarModelo(delegate (TgcMesh unMesh) { unMesh.Rotation = nuevaRotacion; });
-            TransformarModelo(delegate (TgcMesh unMesh) { unMesh.Transform = TGCMatrix.RotationTGCQuaternion(rotation); });
-            */
+            TGCMatrix.RotationTGCQuaternion(rotation);
+            //TransformarModelo(delegate (TgcMesh unMesh) { unMesh.Rotation = nuevaRotacion; });
+            //TransformarModelo(delegate (TgcMesh unMesh) { unMesh.Transform = TGCMatrix.RotationTGCQuaternion(rotation); });
+            rotacionMesh = TGCMatrix.RotationTGCQuaternion(rotation);
         }
+
+        public void aplicarTransformaciones()
+        {
+            TransformarModelo(delegate (TgcMesh unMesh) { unMesh.Transform = baseScaleRotation * rotacionMesh * TGCMatrix.Translation(unMesh.Position); });
+        }
+
 
         public void Render()
         {
@@ -57,7 +65,6 @@ namespace TGC.Group.Model
         {
             TransformarModelo(delegate (TgcMesh unMesh) { unMesh.Dispose(); });
         }
-
 
 
 
--- TGC.Group/Model/Nave.cs
+++ TGC.Group/Model/Nave.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using System.Collections.Generic;
 using System;
 using System.Runtime.CompilerServices;
+using Microsoft.DirectX.Direct3D;
 
 namespace TGC.Group.Model
 {
@@ -25,9 +26,12 @@ namespace TGC.Group.Model
             this.input = input;
         }
 
+
         public void Init()
         {
             modeloNave = new ModeloCompuesto(mediaDir + "XWing\\X-Wing-TgcScene.xml", posicion);
+            var rotacionInicial = new TGCVector3(0f, 1f, 0f) * Geometry.DegreeToRadian(180f);
+            modeloNave.CambiarRotacion(rotacionInicial);
         }
 
 
@@ -50,23 +54,36 @@ namespace TGC.Group.Model
         public void Update(float elapsedTime)
         {
             TGCVector3 direccionDelInput = new TGCVector3(0, 0, 0); //A "direccion" se refiere a direccion y sentido.
+            TGCVector3 giroRotacion = new TGCVector3(0f, Geometry.DegreeToRadian(180f), 0f);
 
             if (input.keyDown(Key.Left) || input.keyDown(Key.A))
             {
                 direccionDelInput.X = -1;
+                giroRotacion.Z = Geometry.DegreeToRadian(20f);
             }
             else if (input.keyDown(Key.Right) || input.keyDown(Key.D))
             {
                 direccionDelInput.X = 1;
+                giroRotacion.Z = Geometry.DegreeToRadian(-20f);
+            }
+            else
+            {
+                giroRotacion.Z = Geometry.DegreeToRadian(0f);
             }
 
             if (input.keyDown(Key.Up) || input.keyDown(Key.W))
             {
                 direccionDelInput.Y = 1;
+                giroRotacion.X = Geometry.DegreeToRadian(10f);
             }
             else if (input.keyDown(Key.Down) || input.keyDown(Key.S))
             {
                 direccionDelInput.Y = -1;
+                giroRotacion.X = Geometry.DegreeToRadian(-10f);
+            }
+            else
+            {
+                giroRotacion.X = Geometry.DegreeToRadian(0f);
             }
 
             if (input.keyDown(Key.LeftShift))
@@ -77,13 +94,19 @@ namespace TGC.Group.Model
             {
                 velocidad -= 0.01f;
             }
+            else
+            {
+
+            }
 
             MoverseEnDireccion(direccionDelInput, elapsedTime);
+            modeloNave.CambiarRotacion(giroRotacion);
 
         }
 
         public void Render()
         {
+            modeloNave.aplicarTransformaciones();
             modeloNave.Render();
             //new TgcText2D().drawText("Posicion de la nave:\n" + posicion.ToString(), 5, 20, Color.White);
             //new TgcText2D().drawText("Rotacion de la nave:\n" + mainMesh.Rotation.ToString(), 5, 100, Color.White);
